e0bcbd1b4c652c860a3869d13d6d90e7925698d9
matheuslima <mathlb.dev@hotmail.com>
2025-03-05 11:58:40 -0300

    Início do desafio

---
+++ src/test/java/com/picpaybackend/PicpaybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
